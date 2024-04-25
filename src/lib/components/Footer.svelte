8d41d99d6a4d16f43cfafa0477967254840c4e0e
mj-linane <19750620+mj-linane@users.noreply.github.com>
2024-04-25 14:16:31 -0400

    Add Footer component with copyright information

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,19 @@
+<!-- Footer -->
+<footer>Made with ♥ by MJ Linane | Copyright © 2024</footer>
+
+<style>
+	footer {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+		background-color: #f8f9fa;
+		border-top: 1px solid #e9ecef;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
