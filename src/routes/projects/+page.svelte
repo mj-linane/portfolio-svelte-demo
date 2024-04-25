30c3da8a7402700a65de00a50f2a2ecc523109da
mj-linane <19750620+mj-linane@users.noreply.github.com>
2024-04-25 14:17:37 -0400

    Add new page for projects with Hero and ProjectCardGrid components

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import ProjectCardGrid from '../../lib/components/ProjectCardGrid.svelte';
+	import Hero from '../../lib/components/Hero.svelte';
+</script>
+
+<section>
+	<Hero title="Projects" />
+	<ProjectCardGrid />
+</section>
