db7f8cb4c523ba54cf7e72ec599503af296c6d62
mj-linane <19750620+mj-linane@users.noreply.github.com>
2024-04-25 14:16:42 -0400

    Add ProjectCard and ProjectCardGrid components

---
+++ src/lib/components/ProjectCard.svelte
@@ -0,0 +1,47 @@
+<!-- Single ProjectCard -->
+<script>
+	export let name;
+	export let description;
+	export let image;
+	export let link;
+</script>
+
+<div class="project-card">
+	<a href={link} target="_blank">
+		<img src={image} alt={name} />
+		<h2>{name}</h2>
+		<p>{description}</p>
+		<img src={image} alt={name} />
+		<h2>{name}</h2>
+		<p>{description}</p>
+	</a>
+</div>
+
+<style>
+	.project-card {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 1rem;
+		border: 1px solid #ccc;
+		border-radius: 8px;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+	}
+
+	img {
+		width: 100%;
+		border-radius: 8px;
+		margin-bottom: 1rem;
+	}
+
+	h2 {
+		font-size: 1.5rem;
+		margin-bottom: 0.5rem;
+	}
+
+	p {
+		font-size: 1rem;
+		margin-bottom: 1rem;
+	}
+</style>
---
+++ src/lib/components/ProjectCardGrid.svelte
@@ -0,0 +1,63 @@
+<script>
+	import ProjectCard from '$lib/components/ProjectCard.svelte';
+	export let itemsPerRow = 3;
+	export let projects = [
+		{
+			name: 'Project 1',
+			description: 'This is a description of project 1',
+			image: 'https://via.placeholder.com/300',
+			link: 'https://google.com'
+		},
+		{
+			name: 'Project 2',
+			description: 'This is a description of project 2',
+			image: 'https://via.placeholder.com/300',
+			link: 'https://google.com'
+		},
+		{
+			name: 'Project 3',
+			description: 'This is a description of project 3',
+			image: 'https://via.placeholder.com/300',
+			link: 'https://google.com'
+		},
+		{
+			name: 'Project 4',
+			description: 'This is a description of project 4',
+			image: 'https://via.placeholder.com/300',
+			link: 'https://google.com'
+		},
+		{
+			name: 'Project 5',
+			description: 'This is a description of project 5',
+			image: 'https://via.placeholder.com/300',
+			link: 'https://google.com'
+		},
+		{
+			name: 'Project 6',
+			description: 'This is a description of project 6',
+			image: 'https://via.placeholder.com/300',
+			link: 'https://google.com'
+		}
+	];
+</script>
+
+<section style="display: grid; grid-template-columns: repeat({itemsPerRow}, 1fr); gap: 1rem;">
+	{#each projects as project}
+		<ProjectCard
+			name={project.name}
+			description={project.description}
+			image={project.image}
+			link={project.link}
+		/>
+	{/each}
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+</style>
