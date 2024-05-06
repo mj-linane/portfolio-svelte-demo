6c279359cb01bf9e3b3efae2b49bb5c9b87584f9
mj-linane <19750620+mj-linane@users.noreply.github.com>
2024-05-06 12:17:44 -0400

    feat: Add LongProductDescription component for displaying a long product description

---
+++ src/lib/components/LongProductDescription.svelte
@@ -0,0 +1,12 @@
+<!-- Component that displays a long product description for a long sales page -->
+
+<script>
+	export let sectionTitle = 'Product Description';
+	export let description =
+		'Ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, vestibulum nunc sit amet, ultrices nunc. Nulla facilisi etiam dignissim diam quis enim lobortis.';
+</script>
+
+<section class="long-product-description">
+	<h2>{sectionTitle}</h2>
+	<p>{description}</p>
+</section>
