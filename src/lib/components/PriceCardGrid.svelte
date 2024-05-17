5751d8696ede80af26b798ef68a82467eaee6e10
mj-linane <19750620+mj-linane@users.noreply.github.com>
2024-05-17 08:33:44 -0400

    refactor: Update PriceCardGrid component to use priceTiers prop for dynamic rendering

--- src/lib/components/PriceCardGrid.svelte
+++ src/lib/components/PriceCardGrid.svelte
@@ -1,3 +1,33 @@
 <script>
 	import PriceCard from '$lib/components/PriceCard.svelte';
+	export let priceTiers = [];
 </script>
+
+<section class="price-cards-container">
+	<div class="price-card-row">
+		{#if priceTiers.length > 0}
+			{#each priceTiers as { priceTier, price, features }}
+				<PriceCard {priceTier} {price} {features} />
+			{/each}
+		{/if}
+	</div>
+</section>
+
+<style>
+	.price-cards-container {
+		background-color: green;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding: 4rem;
+	}
+	.price-card-row {
+		max-width: 80%;
+		border-color: red;
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: space-between;
+		gap: 20px;
+	}
+</style>
